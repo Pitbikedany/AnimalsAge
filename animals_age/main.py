ceeee08d3a8ed0be83dd69888058d3584ffcb8ee
Daniel Santos Lourenço <danysloureco@gmail.com>
2024-09-17 17:29:03 +0100

    Update main.py

--- animals_age/main.py
+++ animals_age/main.py
@@ -1,11 +1,7 @@
-# se o gato tem 1 ano = 15 anos
-# se tem 2 anos = 15 + 9 anos
-# se tem mais de 2 = anos * 4 anos humanos 
-
 def human_years_cat_years_dog_years(human_years):
     if human_years == 1:
         cat_years = 15
-        dog_yeats = 15
+        dog_years = 15
     elif human_years == 2:
         cat_years = 15 + 9 
         dog_years = 15 + 9
@@ -15,5 +11,5 @@ def human_years_cat_years_dog_years(human_years):
          
     return [human_years,cat_years,dog_years]
 
-human_years = int(input('Insira a sua idade: '))
+human_years = int(input('Insert your age: '))
 print(human_years_cat_years_dog_years(human_years))
